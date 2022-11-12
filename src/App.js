e1797d993addee276a59009421f2e64bd203316c
Matias Gonzalez <mng_epson@hotmail.com>
2022-11-12 08:22:08 -0300

    feat: refactor styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './App.css'
 import Gallery from './Gallery';
 function App() {
   return (
    
    <>
-   <h1 style={{textAlign: 'center'}}>Galeria de Imagenes</h1>
-   <h2 style={{textAlign: 'center'}}>Taller de Fotografia (Epja 1431) </h2>
+   <h1 style={{textAlign: 'center'}}>Galeria de Imágenes</h1>
+   <h2 style={{textAlign: 'center'}}>Taller de Fotografía (Epja 1431) </h2>
 <Gallery/>
    </>
   );
